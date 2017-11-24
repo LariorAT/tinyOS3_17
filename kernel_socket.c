2ed631b483b8831e120c792beb7a54866f2ca962
stavrosgreece <stayros_1@yahoo.gr>
2017-11-24 16:58:15 +0200

    the initialization of Sockets

--- kernel_socket.c
+++ kernel_socket.c
@@ -1,6 +1,67 @@
 
 #include "tinyos.h"
+#include "kernel_streams.h"
+#include "kernel_dev.h"
+#include "kernel_proc.h"
+#include "kernel_cc.h"
+#include "kernel_pipeAndSockets.h"
 
+/* The port table */
+SCB* PORT_MAP[MAX_PORT+1];
+
+typedef struct Socket_Control_Block SCB;
+
+typedef struct Unbound_Socket
+{
+	rlnode node;
+}USocket;
+
+typedef struct Listener_Socket
+{
+	CondVar hasRequest;
+	rlnode ReqQueue;
+}LSocket;
+
+typedef struct PEER_SOCKET{
+	SCB* PeerSocket;
+	PPCB* pipeSend;
+	PPCB* pepeReceive;
+}PSocket;
+
+
+typedef enum{
+	UNBOUND_SOCKET,
+	LISTENER_SOCKET,
+	PEER_SOCKET
+}Socket_type;
+
+typedef struct Socket_Control_Block
+{
+	int ref_count;
+	FCB* fcb;
+	port_t port;
+	Socket_type type;
+	union{
+		USocket* USocket;
+		LSocket* LSocket;
+		PSocket* PSocke;
+	};
+}SCB;
+typedef struct Connection_Request
+{
+	CondVar condition;
+	SCB* socket;
+	int accepted;
+	rlnode node;
+}ConReq;
+
+static file_ops socket_fops = 
+{
+  .Open = NULL,
+  .Read = NULL,
+  .Write = NULL,
+  .Close = NULL
+};
 
 Fid_t sys_Socket(port_t port)
 {
