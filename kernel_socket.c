97726d7a4ee38d01f1edf5dec206fa451169312a
stavrosgreece <stayros_1@yahoo.gr>
2017-11-25 15:44:10 +0200

    sys_Socket Test

--- kernel_socket.c
+++ kernel_socket.c
@@ -63,9 +63,46 @@ static file_ops socket_fops =
   .Close = NULL
 };
 
-Fid_t sys_Socket(port_t port)
-{
-	return NOFILE;
+Fid_t sys_Socket(port_t port )
+{	
+	//fprintf(stderr, "%d\n",port );
+
+	// WE ARE USING 0, TB Reconsidered
+	/** Check if it is a valid port*/
+	if (port > MAX_PORT || port < 0){  
+		fprintf(stderr, "NOFILE\n" );
+		return NOFILE;
+	}
+	
+	FCB** fcb = xmalloc(sizeof(FCB*));
+	Fid_t* fid = xmalloc(sizeof(Fid_t));
+
+
+	if(FCB_reserve(1,fid,fcb) == 0){
+		fprintf(stderr, "NOFILE\n" );
+		return NOFILE;
+	}
+
+	fprintf(stderr, "SOcket1\n" );
+	SCB* s = xmalloc(sizeof(SCB));
+	s->ref_count = 1;
+	s->fcb = fcb[0];
+	
+
+	s->port = port;
+	s->type = UNBOUND_SOCKET;
+	s->USocket = xmalloc(sizeof(USocket));
+	 
+	rlnode_init(& s->USocket->node,s);
+
+	fcb[0]->streamobj = s;
+	fcb[0]->streamfunc = &socket_fops;
+	
+	
+	//fprintf(stderr, "%x\n",fcb[0] );
+	//fprintf(stderr, "%d\n",fid[0] );
+	return fid[0];
+	
 }
 
 int sys_Listen(Fid_t sock)
